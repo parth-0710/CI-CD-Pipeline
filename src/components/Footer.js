669237c64bad673d6fcb1951e03ca2f33e573ff9
PARTH TRIVEDI <99486819+parth-0710@users.noreply.github.com>
2023-07-09 13:54:10 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,7 +11,7 @@ export default function Footer() {
       <Navbar fixed="bottom" bg="dark" variant="dark">  
         <Container  className="text-center text-muted">
             <Col lg={12} className="text-center text-light">
-            {year} - All Rights are already purchased by Parth Mangukiya DevOps....
+            {year} - All Rights are already purchased by Parth Trivedi DevOps....
             </Col>
         </Container>
       </Navbar>
